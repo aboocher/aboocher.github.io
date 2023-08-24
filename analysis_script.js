3f3b9f6b1823d16128831fe72965a375704a22f7
aboocher <aboocher@gmail.com>
2023-08-24 14:22:45 -0700

    updating some stuff
    updating some stuff!

--- analysis_script.js
+++ analysis_script.js
@@ -1,3 +1,21 @@
+let number_of_questionsElement = 0;
+const number_buttonElement = document.getElementById("number_button");
+const totalElement = document.getElementById("total");
+var divvv = document.getElementById("divcontainer");
+var divvv0 = document.getElementById("divcontainer0");
+var adam;
+function init() {
+    number_buttonElement.addEventListener("click", () => {
+        adam = parseInt(document.getElementById("number_of_questions").value);
+        divvv.style.display = "block";
+        divvv0.style.display = "none";
+        startQuiz();
+    });
+}
+
+init()
+
+
 const questions = [
     {
         question: "You are given that $S$ is a set of real numbers and that $M$ is an upper bound for $S$.  Is it necessarily true that for all $s\\in S$, $$s \\leq 2M?$$",
@@ -38,22 +56,31 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 const scoreElement = document.getElementById("score_number");
-
+const remainingElement = document.getElementById("remaining_number")
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz() {
+async function startQuiz() {
+    await init();
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    };
+}
 
 function showQuestion() {
     resetState();
+    totalElement.innerHTML = "" + adam;
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    remainingElement.innerHTML = adam - currentQuestionIndex;
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
@@ -86,12 +113,6 @@ function selectAnswer(e) {
     nextButton.style.display = "block";
 }
 
-function resetState() {
-    nextButton.style.display = "none";
-    while (answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    };
-}
 
 nextButton.addEventListener("click", () => {
     if (currentQuestionIndex < questions.length) {
@@ -117,4 +138,3 @@ function showScore() {
     nextButton.style.display = "block";
 }
 
-startQuiz()
